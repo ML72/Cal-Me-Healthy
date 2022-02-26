0c73a9f065b5d0e232dd9cf8bd27a3734bcedb2c
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-02-26 08:53:15 -0800

    Update settings.js

--- server/settings.js
+++ server/settings.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://TestUser:iLxdV4NGko3gbpTg@cluster0.wmikr.mongodb.net/Test?retryWrites=true&w=majority";
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
+const LOGMEAL_TOKEN = process.env.LOGMEAL_TOKEN || 'Bearer 09742fa69f23bafc255965a1d317a85029923d1d'
 
 module.exports = { PORT, MONGO_URI, JWT_SECRET };
