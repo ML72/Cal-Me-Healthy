d72577ef03dcd162c5b38689fecbcdde28b7a9f3
emma h <emmahuang37@gmail.com>
2022-02-25 22:30:53 -0800

    added overall g/p theme
    font switch to Inter, primary/secondary/error colors edited, spacing + borderRadius increased

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,44 @@ import Dashboard from './components/pages/Dashboard';
 import Signup from './components/pages/Signup';
 import Signin from './components/pages/Signin';
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 if(localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
+
+const ctheme = createTheme({
+  palette: {
+    primary: {
+      // green primary
+      main: "#01B763"
+    },
+    secondary: {
+      // purple secondary
+      main: "#6e62f9"
+    },
+    error: {
+      // red error
+      main: '#f4365d',
+    },
+  },
+  spacing: 10,
+  divider: "rgba(0,55,30,0.31)",
+  shape: {
+    borderRadius: 6
+  },
+  typography: {
+    fontSize: 14,
+    fontFamily: '"Inter", "Helvetica", "Arial", sans-serif',
+    fontWeightBold: 800
+  }
+});
+
 const App = () => {
 
   return (
+    <ThemeProvider theme={ctheme}>
     <BrowserRouter>
       <Navbar />
       <Routes>
@@ -23,6 +54,7 @@ const App = () => {
         <Route exact path="/signin" element={<Signin />} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
