4877f712d0b3ceb3b6cd0a27391198ba85edd77e
Michael <michael.mingdao.li@gmail.com>
2022-02-25 23:09:03 -0800

    Redux authentication system code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
+// Componentss
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/pages/Dashboard';
 import Signup from './components/pages/Signup';
 import Signin from './components/pages/Signin';
+import NotFound from './components/pages/NotFound';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -14,15 +21,22 @@ if(localStorage.token) {
 
 const App = () => {
 
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/signin" element={<Signin />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
