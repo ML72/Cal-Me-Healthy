fd5863302a1cf60c5a87c811f750e13e4fc2a8a5
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-02-26 08:20:55 -0800

    Update Dashboard.js

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,13 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
 
 const Dashboard = () => {
 
+  const inputFile = useRef(null);
+
+  const onChangeFile = (event) => {
+    const data = new FormData();
+    data.append('image', event.target.files[0])
+    callAPI(data);
+  }
+
+  const onButtonClick = () => {
+    inputFile.current.click();
+  };
+
+  function callAPI(fileData) {
+      const headers = {
+              'Authorization': 'Bearer 09742fa69f23bafc255965a1d317a85029923d1d',
+              'Content-Type': 'multipart/form-data',
+            }
+
+          var request = axios.post(
+            "https://api.logmeal.es/v2/image/recognition/dish", 
+            fileData, 
+            {headers : headers}
+          )
+          request.then(response => {
+            try {
+              console.log(response.data.recognition_results[0].name);
+            }
+            catch {
+              console.error(response.error)
+            }
+          })
+  }
+  
   return (
     <Fragment>
       <Typography variant="h1" component="div" align="center" sx={{ my: "1rem" }}>
         Dashboard
       </Typography>
+      <input type='file' id='file' ref={inputFile} style={{display: 'none'}} onChange={onChangeFile}/>
+      <button onClick={onButtonClick}>Open file upload window</button>
     </Fragment>
   );
 }
