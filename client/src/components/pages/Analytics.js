3a6230811064fce98355de73d2dec9476ce76840
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-02-26 16:12:17 -0800

    History/Analytics files
    (They do nothing as of yet I'm just not going to use them so I want to have them backed up)

---
+++ client/src/components/pages/Analytics.js
@@ -0,0 +1,14 @@
+import React, { Fragment, useRef } from 'react';
+import Typography from '@mui/material/Typography';
+import { Link, Navigate } from 'react-router-dom';
+
+const Analytics = () => {
+
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default Analytics;
